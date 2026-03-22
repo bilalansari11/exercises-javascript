4704841144ed84a23ee06130a3b08ee0a953a452
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-03-22 11:08:52 +0500

    Practice of Alerts

---
+++ Chapter-1 (Alert)/Chapter-1/app.js
@@ -0,0 +1,14 @@
+//   1
+// alert("Welcome to my Website!");
+
+//   2
+// alert("Error! Please enter a valid password.");
+
+//   3
+// alert("Welcome to JS Land... \n Happy Cooding!");   // \n is used for line-breake
+
+//   4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this page from creating addition dialogs.")
+
+// Completed Exercises of Chapter 1
