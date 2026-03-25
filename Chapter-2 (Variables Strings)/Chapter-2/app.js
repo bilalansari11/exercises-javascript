2bb1e90324610ddb2b362b2e8fadaa2bd944563d
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-03-25 12:01:41 +0500

    Practice of Variables with string values

---
+++ Chapter-2 (Variables Strings)/Chapter-2/app.js
@@ -0,0 +1,53 @@
+//   1
+// var username;
+
+//   2
+// var myName = "Bilal Ahmed Ansari";
+
+//   3
+// var message = "Hello World";
+
+// alert(message);
+
+//   4
+// var userName = "John Doe";
+// var age = "15 years old";
+// var qualification = "Certified Mobile Application Development";
+
+// alert(userName);
+// alert(age);
+// alert(qualification);
+
+//   5
+// var food = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+// alert(food);
+
+//   6 
+// var email = "example@example.com";
+
+// alert("My email address is " + email);
+
+//   7 
+
+// var book = "A smarter way to learn JavaScript.";
+
+// alert("I am trying to learn from the Book " + book);
+
+//   8
+
+// document.write("Yah! i can write HTML content through JavaScript");
+
+//   9
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// a) in alert
+
+// alert(design);
+
+// b) in browser
+
+// document.write(design);
+
+//  Completed Exercises of Chapter 2
