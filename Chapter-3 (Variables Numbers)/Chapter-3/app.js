bed454730178fc63722dd67ef1cee2814d78c6a8
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-03-26 21:02:41 +0500

    Practice of variables with number values

---
+++ Chapter-3 (Variables Numbers)/Chapter-3/app.js
@@ -0,0 +1,80 @@
+//   1
+
+// var age = 24;
+
+// alert(age);
+
+//   2
+
+// var num = 1;
+
+// alert("You have visited " + num + " time");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+// num = ++num;
+
+// alert("You have visited " + num + " times");
+
+//   3
+
+// var birthYear = 2001;
+
+// document.write("My birth year is " + birthYear);
+// document.write("<p>Data type of my declared variable is number</p>");  
+
+//   4
+
+// var userName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+
+// document.write("<b>" + userName + "</b>" + " ordered " + "<b>" + quantity + " " + productTitle + "</b>" + "'s" + " on XYZ Clothing store");
+
+//   Completed Exercises of Chapter 3
