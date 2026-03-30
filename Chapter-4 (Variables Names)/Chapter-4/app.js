a9c957116d42bf0448240c9b990770bb735b9d4c
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-03-31 01:28:56 +0500

    practice of variable names (e.g: correct and incorrect types)

---
+++ Chapter-4 (Variables Names)/Chapter-4/app.js
@@ -0,0 +1,38 @@
+//   1
+// var x = 10, y = 20, z = 25;
+
+// console.log(x, y, z);
+
+//   2
+
+//  Legal Variable Names 
+
+// var myName = "Bilal";
+
+// var $lastname = "Ansari";
+
+// var _myage = 24;
+
+// console.log("My name is " + myName + " " + $lastname + "." + " I am " + _myage + " years old.");
+
+//  Illegal Variable Names
+
+// var @myname = "Bilal";
+
+// var &age = 24;
+
+// console.log("My name is " + @myname + ". " + "I am " + &age + " years old.");
+
+//   3
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+
+// document.write("<p>Varable names can only contain numbers, $ and _. For example: $my_1stVariable</p>");
+
+// document.write("<p>Variable must begin with a letter, $ or _. For Example: $name, _name, name</p>")
+
+// document.write("<p>Variable names are case sensitive</p>");
+
+// document.write("<p>Variable names should not be JS keywords</p>");
+
+//   Completed Exercises of Chapter 4
