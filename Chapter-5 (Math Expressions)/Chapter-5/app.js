c69b715d200df9a5f16a4e772dc7586a3810b7c8
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-04-02 00:09:22 +0500

    practice of mathematical expressions

---
+++ Chapter-5 (Math Expressions)/Chapter-5/app.js
@@ -0,0 +1,187 @@
+//   1
+
+// var a = 40;
+
+// var b = 10;
+
+// var c = a + b;
+
+// document.write("<p>" + "Sum of " + a + " + " + b + " is " +  c + "</p>");
+
+//   2
+
+// var c = a - b;
+
+// document.write("<p>" + "Subtraction of " + a + " - " + b + " is " + c + "</p>");
+
+// var c = a * b;
+
+// document.write("<p>" + "Multiplication of " + a + " x " + b + " is " + c + "</p>");
+
+// var c = a / b;
+
+// document.write("<p>" + "Division of " + a + " / " + b + " is " + c + "</p>");
+
+// var c = a % b;
+
+// document.write("<p>" + "Modulus of " + a + " % " + b + " is " + c + "</p>");
+
+// I CAN USE " + " FOR CONCATINATION
+
+//    3
+
+// var num;
+
+// document.write("<p>" + "Value after declaration is ", num + "</p>");
+
+// num = 10;
+
+// document.write("<p>" + "Initial value: ", num , "</p>");
+
+// num = ++num;
+
+// document.write("<p>" , "Value after increament is: ", num , "</p>");
+
+// num = num + 5;
+
+// document.write("<p>" , "Value after addition is: ", num, "</p>");
+
+// num = --num;
+
+// document.write("<p>", "Value after decrement is: ", num, "</p>");
+
+// num = num / 3;
+
+// document.write("<p>", "The reminder is: ", num, "</p>");
+
+// I CAN USE " , " FOR CONCATINATION/DISTINGUISH
+
+//   4
+
+// var singleTicket = 900;
+
+// var $5Tickets = singleTicket * 5;
+
+// document.write("<p>Total cost to buy 5 tickets is " + $5Tickets + " PKR" + "</p>");
+
+//   5
+
+// let multiplier = 11;
+// let num = 1;
+
+// document.write("<h1>Table of 11</h1>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+// document.write("<p>" + multiplier + " x " + num + " = " + multiplier*num++ + "</p>");
+
+//   6
+
+// Not Completed
+
+
+//   7
+
+// document.write("<h1>" + "Shopping Cart" + "</h1>");
+
+// let priceOfItem1 = 650;
+// let qtyOfItem1 = 3;
+
+// let priceOfItem2 = 100;
+// let qtyOfItem2 = 7;
+
+// let shippingCharges = 100;
+
+// let total = (priceOfItem1*3) + (priceOfItem2*7) + (100)
+
+// document.write("<p>" + "Price of item 1 is " + priceOfItem1 + "</p>");
+// document.write("<p>" + "Quantity of item 1 is " + qtyOfItem1 + "</p>");
+// document.write("<p>" + "Price of item 2 is " + priceOfItem2 + "</p>");
+// document.write("<p>" + "Quantity of item 2 is " + qtyOfItem2 + "</p>");
+// document.write("<p>" + "Shipping Charges " + shippingCharges + "</p>");
+
+// document.write("<p>" + "Total cost of your order is " + total + "</p>");
+
+//   8
+
+// let totalMarks = 550;
+// let marksObtained = 456;
+
+// let percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("<p>" + "Total marks: " + totalMarks + "</p>");
+// document.write("<p>" + "Marks obtained: " + marksObtained + "</p>");
+// document.write("<p>" + "Percentage: " + percentage + "%" + "</p>");
+
+//   9
+
+// let usDollars = 10;
+// let oneDollar = 104.80;
+
+// let saudiRiyals = 25;
+// let oneRiyal = 28;
+
+// let total = (usDollars * oneDollar) + (saudiRiyals * oneRiyal)
+
+// document.write("<p>" + "Total currency in PKR: " + total + "</p>");
+
+
+//   10
+
+// let num = 11;
+
+// let arithmaticExpressions = num + 5 * 10 / 2;
+
+// document.write("<p>" + arithmaticExpressions + "</p>");
+
+//   11
+
+// document.write("<h1> Age Calculator </h1>");
+
+// let currentYear = 2026;
+
+// let birthYear = 2001;
+
+// let yourAge = currentYear - birthYear;
+
+// document.write("<p>" + "Current year: " + currentYear + "</p>");
+// document.write("<p>" + "Birth year: " + birthYear + "</p>");
+// document.write("<p>" + "Your age is: " + yourAge + "</p>");
+
+//   12
+
+// document.write("<h1> The Geometrizer </h1>");
+
+// let radius = 20;
+// let circumference = 2 * 3.142 * radius;
+// let area = 3.142 * radius * radius;
+
+// document.write("<p>" + "Radius of a circle: " + radius + "</p>");
+// document.write("<p>" + "The circumference is " + circumference + "</p>");
+// document.write("<p>" + "The area is " + area + "</p>");
+
+//   13
+
+// document.write("<h1> The Lifetime Supply Calculator </h1>");
+
+// let favoriteSnack = "Noms";
+// let currentAge = 18;
+// let maximumAge = 99;
+// let snacksPerDay = 3;
+
+// let totalEatForRestOfLife = snacksPerDay * maximumAge - currentAge * snacksPerDay;
+
+// document.write("<p>" + "Favorite Snack: " + favoriteSnack + "</p>");
+// document.write("<p>" + "Current Age: " + currentAge + "</p>");
+// document.write("<p>" + "Estimated Maimum Age: " + maximumAge + "</p>");
+// document.write("<p>" + "Amount of Snacks per day: " + snacksPerDay + "</p>");
+// document.write("<p>" + "You will need " + totalEatForRestOfLife + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge + "</p>");
+
+
+//   Completed Exercises of Chapter 5 (except part 6)
