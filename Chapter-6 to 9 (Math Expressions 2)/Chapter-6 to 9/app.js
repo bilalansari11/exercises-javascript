e406abc2e97518158bf740a89fe040959833866a
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-04-04 18:42:12 +0500

    practice of mathematics expressions (increaments, decrements etc)

---
+++ Chapter-6 to 9 (Math Expressions 2)/Chapter-6 to 9/app.js
@@ -0,0 +1,100 @@
+//   1
+
+// var a = 9;
+
+// document.write("<p>" + "Result:" + "</p>");
+// document.write("<p>" + "The value of a is: " + a + "</p>");
+
+// document.write("<p>" + "The value of ++a is: " + ++a + "</p>");
+// document.write("<p>" + "Now the value of a is: " + a + "</p>");
+
+// document.write("<p>" + "The value of a++ is: " + a++ + "</p>");
+// document.write("<p>" + "Now the value of a is: " + a + "</p>");
+
+// document.write("<p>" + "The value of --a is: " + --a + "</p>");
+// document.write("<p>" + "Now the value of a is: " + a + "</p>");
+
+// document.write("<p>" + "The value of a-- is: " + a-- + "</p>");
+// document.write("<p>" + "Now the value of a is: " + a + "</p>");
+
+//   2
+
+// var a = 2;
+// var b = 1;
+
+// document.write("<p>" + "a is: " + a + "</p>");
+
+// document.write("<p>" + "b is: " + b + "</p>");
+
+// var result = --a - --b + ++b + b--;
+
+// document.write("<p>" + "Result is: " + result +"</p");
+
+
+//   3
+
+// var userName = prompt("Enter your name!");
+
+// document.write("Welcome " + userName + " !");
+
+
+//   5
+
+// var userInput = prompt("Enter number to display you a maltiplication table of that number");
+
+// var otherNumber = 5;
+
+// var multiplier = 1;
+
+// if (userInput === "") {
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+//     document.write("<p>" + otherNumber + " x " + multiplier + " = " + otherNumber * multiplier++ + "</p>");
+// } else {
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+//     document.write("<p>" + userInput + " x " + multiplier + " = " + userInput * multiplier++ + "</p>");
+// }
+
+
+//   6
+
+// var subject1 = prompt("Enter your 1st Subject?");
+// var subject2 = prompt("Enter your 2nd Subject?");
+// var subject3 = prompt("Enter your 3rd Subject?");
+
+// var totalMarksSubject = 100;
+
+// var marksInSubject1 = +prompt("Enter your " + subject1 + " Marks?");
+// var marksInSubject2 = +prompt("Enter your " + subject2 + " Marks?");
+// var marksInSubject3 = +prompt("Enter your " + subject3 + " Marks?");
+
+// var totalMarks = totalMarksSubject * 3;
+
+// var obtainedMarks = marksInSubject1 + marksInSubject2 + marksInSubject3;
+
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// document.write("<table>");
+// document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>");
+// document.write("<tr><td>" + subject1 + "</td><td>" + totalMarksSubject + "</td><td>" + marksInSubject1 + "</td><td>" + (marksInSubject1/totalMarksSubject) * 100 + " % " + " </td></tr>");
+// document.write("<tr><td>" + subject2 + "</td><td>" + totalMarksSubject + "</td><td>" + marksInSubject2 + "</td><td>" + (marksInSubject2/totalMarksSubject) * 100 + " % " + " </td></tr>");
+// document.write("<tr><td>" + subject3 + "</td><td>" + totalMarksSubject + "</td><td>" + marksInSubject3 + "</td><td>" + (marksInSubject3/totalMarksSubject) * 100 + " % " + " </td></tr>");
+// document.write("<tr><th></th><th> " + totalMarks + "</th><th>" + obtainedMarks + "</th><th>" + percentage + " % " + "</th></tr>");
+// document.write("</table>");
+
+// Completed Exercises of Chapter 6
