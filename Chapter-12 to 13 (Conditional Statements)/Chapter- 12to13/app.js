f82e380c9056cd59393d7eaf24477f242337954f
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-04-10 04:49:18 +0500

    practice of if...else & else if statements

---
+++ Chapter-12 to 13 (Conditional Statements)/Chapter- 12to13/app.js
@@ -0,0 +1,98 @@
+//   1
+
+
+//   2
+
+// var number1 = 30;
+// var number2 = 30;
+
+// if (number1 > number2) {
+//     console.log(number1 + " is greater");
+// } else if(number1 < number2) {
+//     console.log(number2 + " is greater");
+// } else {
+//     console.log("Numbers are equal/same");
+// }
+
+//   3
+
+// var userInput = +prompt("Enter any number!");
+
+// if (userInput > 0) {
+//     alert("The number is positive " + userInput);
+// } else if(userInput < 0) {
+//     alert("The number is negative " + userInput);
+// } else {
+//     alert("The number is zero " + userInput);
+// }
+
+//  4
+
+// var character = "c";
+
+// if (character == "a") {
+//     console.log("true");
+// } else if (character == "e") {
+//     console.log("true");
+// } else if (character == "i") {
+//     console.log("true");
+// } else if (character == "o") {
+//     console.log("true");
+// } else if (character == "u") {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+//  5
+
+// var correctPassword = "qwert12345";
+
+// var userPassword = prompt("Enter your password!");
+
+// if (userPassword == "") {
+//     alert("Plaese enter your password!");
+// } else if (userPassword == correctPassword) {
+//     alert("Correct! The password you entered matches the original password")
+// } else {
+//     alert("Incorrect password !")
+// }
+
+//  6
+
+// var greeting;
+
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+//     console.log(greeting);
+// } else {
+//     greeting = "Good evening";
+//     console.log(greeting);
+// }
+
+//  7
+
+// var message;
+
+// var time = +prompt("Enter time !");
+
+// if (time >= 0 && time < 1200) {
+//     message = "Good Morning!"
+//     console.log(message);
+// } else if (time >= 1200 && time < 1700) {
+//     message = "Good afternoon";
+//     console.log(message);
+// } else if (time >= 1700 && time < 2100) {
+//     message = "Good evening";
+//     console.log(message);
+// } else if (time >= 2100 && time <= 2359) {
+//     message = "Good night"
+//     console.log(message);
+// } else {
+//     message = "wrong time";
+//     console.log(message);
+// }
+
+//  Complete exercises except part 1
